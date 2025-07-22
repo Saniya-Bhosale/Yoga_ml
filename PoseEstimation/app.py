210144df40e2254463c131a87feef6cd0eef325d
TejasD6504 <156317024+TejasD6504@users.noreply.github.com>
2025-07-22 23:12:24 +0530

    Update app.py

--- PoseEstimation/app.py
+++ PoseEstimation/app.py
@@ -46,7 +46,7 @@ def index():
     return "Hello from Flask!"
 
 @app.route('/index')
-def index():
+def index_page():
     return render_template('index.html')
 
 @app.route('/analyze-image', methods=['POST'])
@@ -163,7 +163,7 @@ def calculate_accuracy_score(feedback, is_correct):
 #     port = int(os.environ.get("PORT", 5000))
 #     app.run(debug=False, host='0.0.0.0', port=port)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))     # Default to 5000
-    host = os.environ.get('HOST', '0.0.0.0')      # Default to 0.0.0.0
-    app.run(host=host, port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 5000))     # Default to 5000
+#     host = os.environ.get('HOST', '0.0.0.0')      # Default to 0.0.0.0
+#     app.run(host=host, port=port)
