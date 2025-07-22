d06b4a2482cd467605dd6abbf1d47dfada875355
TejasD6504 <156317024+TejasD6504@users.noreply.github.com>
2025-07-22 20:53:15 +0530

    Update app.py

--- PoseEstimation/app.py
+++ PoseEstimation/app.py
@@ -155,4 +155,6 @@ def calculate_accuracy_score(feedback, is_correct):
     return score
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
